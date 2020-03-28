bd436c133a65479e9d340398341802016a69b6e3
Maurizio Rinder <moonrider1510@gmail.com>
2020-03-28 17:10:04 +0100

    make database setup work with init-script

--- tooling/init-user-db.sh
+++ tooling/init-user-db.sh
@@ -4,5 +4,5 @@ set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE USER expandor;
     CREATE DATABASE expensionrecorder;
-    GRANT ALL PRIVILEGES ON DATABASE expandor TO expensionrecorder;
+    GRANT ALL PRIVILEGES ON DATABASE expensionrecorder TO expandor;
 EOSQL
