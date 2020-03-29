a7b95ea927c17c870b9914c5b1df4e28a8003b68
Maurizio Rinder <moonrider1510@gmail.com>
2020-03-30 00:32:48 +0200

    first setup for a test-database

---
+++ src/test/java/com/moonrider/TestDatabaseConnectionTest.java
@@ -0,0 +1,23 @@
+package com.moonrider;
+
+import java.sql.SQLException;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import io.agroal.api.AgroalDataSource;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class TestDatabaseConnectionTest {
+
+	@Inject
+	AgroalDataSource dataSource;
+	
+    @Test
+    public void testDatabaseConnection() throws SQLException {
+    	dataSource.getConnection();
+    }
+
+}
